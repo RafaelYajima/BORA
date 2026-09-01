0ff345063075426c9b2632b8053d605f43b00bee
Rafaelyajima <rafaelyajima2@gmail.com>
2026-08-31 21:40:25 -0300

    Funcionando na porta 3306 e 3307

--- config/conexao.php
+++ config/conexao.php
@@ -1,14 +1,21 @@
 <?php
 
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $password = "";
 $database = "sistema_eventos";
 
-$conexao = mysqli_connect($host, $user, $password, $database);
+// 1. Conexao na faculdade (IFSP porta 3307)
+$conexao = @mysqli_connect($host, $user, $password, $database, 3307);
 
+// 2. Conexao padrao do xampp(casa)
 if (!$conexao) {
-    die("Erro de conexão: " . mysqli_connect_error());
+    $conexao = @mysqli_connect($host, $user, $password, $database, 3306);
+}
+
+// 3. Se ambos falharem, exibe o erro definitivo
+if (!$conexao) {
+    die("Erro de conexão definitivo: " . mysqli_connect_error());
 }
 
 ?>
